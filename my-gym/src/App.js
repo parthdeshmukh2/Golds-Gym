338d21ac7a364977550245e9c25cebff2524f32a
parthdeshmukh2 <parthdeshmukh99@gmail.com>
2022-07-06 22:19:32 +0530

    navbar completed

--- my-gym/src/App.js
+++ my-gym/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Box} from "@mui/material"
+import {Routes, Route} from "react-router-dom"
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import ExersiceDetail from './Pages/ExerciseDetail';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+   <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
+     <Navbar/>
+     <Routes>
+       <Route path="/" element ={<Home/>} />
+       <Route path="/exercise/:id" element ={<ExersiceDetail/>} />
+
+     </Routes>
+     <Footer/>
+
+   </Box>
   );
 }
 
